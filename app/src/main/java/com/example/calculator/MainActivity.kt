eee3cfb2b8aa2aad0035ca4ca5035ec72a80cb1f
Arpit-Butani <148706537+Arpit-Butani@users.noreply.github.com>
2024-04-13 13:39:37 +0530

    add clear and decimal functionality

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var tvResult:TextView
+    var lastNumeric:Boolean = false
+    var lastDot:Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,5 +21,18 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View) {
         tvResult.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
+    }
+
+    fun onClear(view: View) {
+        tvResult.text = ""
+    }
+    fun onDecimal(view: View){
+        if (lastNumeric && !lastDot){
+            tvResult.append(".")
+            lastDot = true
+            lastNumeric = false
+        }
     }
 }
