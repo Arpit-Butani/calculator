462482d92a5db56fdf16f050065a7cb1f8cbb984
Arpit-Butani <148706537+Arpit-Butani@users.noreply.github.com>
2024-04-13 12:00:57 +0530

    Change in Design and add click function onDigit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvResult:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+
+        tvResult= findViewById(R.id.result_tv)
+
+
+    }
+
+    fun onDigit(view: View) {
+        tvResult.append((view as Button).text)
     }
 }
